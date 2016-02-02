7ee06da6623d2ec2e0f4587989fd29df947c59d1
Jin An <sa190@duke.edu>
2016-02-02 11:39:36 -0500

    XML Extract Info

--- CellSociety/src/XMLReader.java
+++ CellSociety/src/XMLReader.java
@@ -1,19 +1,38 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XMLReader {
 
-	public void readXMLFile(File file){
+	public List<String> readXMLFile(File file){
+		List<String> simInfo = new ArrayList<String>();
 		try{
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(file); 
 			doc.getDocumentElement().normalize();
 			
+			NodeList nList = doc.getElementsByTagName("simulation");
+			for (int i=0; i<nList.getLength(); i++){
+				Node nNode = nList.item(i);
+				if(nNode.getNodeType() == Node.ELEMENT_NODE){
+					Element eElement = (Element) nNode;
+					simInfo.add(eElement.getElementsByTagName("name").item(0).getTextContent());
+					simInfo.add(eElement.getElementsByTagName("title").item(0).getTextContent());
+					simInfo.add(eElement.getElementsByTagName("author").item(0).getTextContent());
+					simInfo.add(eElement.getElementsByTagName("settings").item(0).getTextContent());
+					simInfo.add(eElement.getElementsByTagName("dimension").item(0).getTextContent());
+					simInfo.add(eElement.getElementsByTagName("Initial").item(0).getTextContent());
+				}
+			}
 			//name of simulation
 			//title for simulation
 			//author of simulation
@@ -24,5 +43,6 @@ public class XMLReader {
 		catch (Exception e){
 			e.printStackTrace();
 		}
+		return simInfo;
 	}
 }
