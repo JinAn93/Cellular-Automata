83a82963a63c9012b42dafdcd3921498f33bcec0
HUIJIA YU <hy91@duke.edu>
2016-02-04 12:49:51 -0500

    fixed fillcellgrid, fire rules, new test case

--- CellSociety/src/SpreadingFireRules.java
+++ CellSociety/src/SpreadingFireRules.java
@@ -17,15 +17,15 @@ public class SpreadingFireRules extends SimulationRules {
 //		}
 //		else 
 			if (checkState(curr, BURNING)) {
+				System.out.println("+");
 			return EMPTY;
 		}
-		else if (checkState(curr, TREE) &&
-				checkBurning(neighbors[0], neighbors[1], neighbors[2], neighbors[3])) {
-			if (getRand().nextDouble() < probCatch) {
-				return TREE;
+		else if (checkState(curr, TREE)) {			
+			if (checkBurning(neighbors[0], neighbors[1], neighbors[2], neighbors[3]) && getRand().nextDouble() > probCatch) {
+				return BURNING;
 			}
 			else {
-				return BURNING;
+				return TREE;
 			}
 		}
 		return 0;
