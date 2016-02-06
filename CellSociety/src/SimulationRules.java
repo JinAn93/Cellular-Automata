e55de91f8c87589f2f558dea95a11ce4b0fd8ad3
Joseph Lilien <jrl48@duke.edu>
2016-02-05 21:12:03 -0500

    commented and refactored SimulationRules

--- CellSociety/src/SimulationRules.java
+++ CellSociety/src/SimulationRules.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 
+/**
+ * SimulationRules serves to provide the basic methods and functionality for the implementation
+ * of each simulation. Many of its methods interact with CellManager's grid of cells that
+ * represents the state of every active cell in the simulation at any given point. This class
+ * is to be inherited by the individual rules subclasses, one for each type of simulation that the
+ * program can run, where more specific methods and properties are defined.
+ * 
+ * @author Joseph Lilien
+ * @author Jin An
+ * @author Huijia Yu
+ *
+ */
 public abstract class SimulationRules {
 
     public static final int OPEN_NEXT = -2;
@@ -10,6 +22,13 @@ public abstract class SimulationRules {
     public SimulationRules () {
     }
 
+    /**
+     * Called at every step of the simulation's Timeline. This method iterates through CellManager's
+     * grid of cells to determine and set the next state of each cell in the simulation of every
+     * active cell.
+     * 
+     * @param cellGrid
+     */
     protected void applyRule (Cell[][] cellGrid) {
         for (int i = 1; i < cellGrid.length - 1; i++) {
             for (int j = 1; j < cellGrid[0].length - 1; j++) {
@@ -24,6 +43,14 @@ public abstract class SimulationRules {
         }
     }
 
+    /**
+     * Initializes CellManager's cell grid with the initial states of every cell in the simulation,
+     * as defined by the XML configuration file. Also sets the next state property of each cell to
+     * OPEN_NEXT as an initial value.
+     * 
+     * @param cellGrid
+     * @param entry
+     */
     public void fillCellgrid (Cell[][] cellGrid, int[] entry) {
         int count = 0;
         for (int j = 1; j < cellGrid[0].length - 1; j++) {
@@ -35,30 +62,75 @@ public abstract class SimulationRules {
         }
     }
 
+    /**
+     * Abstract method to be implemented in each SimulationRules subclass. Specific implementation
+     * represents the way that updates are handled according to each simulation's set of rules.
+     * 
+     * @param curr
+     * @param neighbors
+     * @param grid
+     * @return -int representing next state of cell
+     */
     protected abstract int findNextState (Cell curr,
                                           Cell[] neighbors,
                                           Cell[][] grid);
 
+    /**
+     * Abstract method to be implemented by each specific SimulationRules subclass. Sets constants
+     * of simulation based on simParams, a string array read in from the XML configuration file.
+     * 
+     * @param simParams
+     */
     protected abstract void setSimulationParameters (String[] simParams);
 
     protected Random getRand () {
         return new Random();
     }
 
+    /**
+     * Returns true if the current state of the cell is the same as the int state argument passed in
+     * 
+     * @param curr
+     * @param state
+     * @return
+     */
     protected boolean checkState (Cell curr, int state) {
         return (curr.getCurrState() == state);
     }
 
+    /**
+     * Returns true if the next state of the cell is the same as the int state argument passed in
+     * 
+     * @param curr
+     * @param state
+     * @return
+     */
     protected boolean checkNextState (Cell curr, int state) {
         return (curr.getNextState() == state);
     }
 
+    /**
+     * Returns the percent of like neighbors. This is determined as the number of similar neighbors
+     * divided by the number of total neighbor states that are not defined as EMPTY
+     * 
+     * @param curr
+     * @param neighbors
+     * @return
+     */
     protected double calculateLikeNeighborsPercent (Cell curr, Cell[] neighbors) {
         double likeCount = countNeighborState(neighbors, curr.getCurrState());
         double occupiedNeighbors = neighbors.length - countNeighborState(neighbors, EMPTY);
         return likeCount / occupiedNeighbors;
     }
 
+    /**
+     * Returns a double equal to the number of neighbors of a certain cell that are the same as the
+     * given state that is passed into the method.
+     * 
+     * @param neighbors
+     * @param state
+     * @return
+     */
     protected double countNeighborState (Cell[] neighbors, int state) {
         double count = 0;
         for (int i = 0; i < neighbors.length; i++) {
@@ -80,9 +152,9 @@ public abstract class SimulationRules {
      */
     public Cell[] setNeighbors (Cell[][] cellGrid, int i, int j) {
         Cell[] neighbors = { cellGrid[i - 1][j], cellGrid[i + 1][j],
-                            cellGrid[i][j + 1], cellGrid[i][j - 1], cellGrid[i - 1][j + 1],
-                            cellGrid[i - 1][j - 1], cellGrid[i + 1][j - 1],
-                            cellGrid[i + 1][j + 1] };
+                             cellGrid[i][j + 1], cellGrid[i][j - 1], cellGrid[i - 1][j + 1],
+                             cellGrid[i - 1][j - 1], cellGrid[i + 1][j - 1],
+                             cellGrid[i + 1][j + 1] };
         return neighbors;
     }
 }
