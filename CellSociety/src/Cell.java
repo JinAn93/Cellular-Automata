d984e906f868fe3140cdcb1aa5eff3b9907dd808
Joseph Lilien <jrl48@duke.edu>
2016-02-08 13:48:18 -0500

    started implementation for antforaging

--- CellSociety/src/Cell.java
+++ CellSociety/src/Cell.java
@@ -1,6 +1,11 @@
+import java.awt.Point;
+import java.util.*;
+
+
 /**
- * Cell serves to contain all the relevant information for each cell. They have been encapsulated by declaring
- * as private variables and using getter and setter methods for each information. 
+ * Cell serves to contain all the relevant information for each cell. They have been encapsulated by
+ * declaring
+ * as private variables and using getter and setter methods for each information.
  * 
  * @author Joseph Lilien
  * @author Jin An
@@ -11,8 +16,46 @@
 public class Cell {
     private int currState = 0;
     private int nextState = 0;
+    // PredatorPrey Properties
     private double turnsOnState = 0;
     private double energy;
+    // ForagingAnts Properties
+    private Cell orientation;
+    private boolean isNest;
+    private boolean isFood;
+    private Integer[] pher = new Integer[2]; //pher[0] = homePher, pher[1] = foodPher
+
+    public Cell getOrientation () {
+        return orientation;
+    }
+
+    public void setOrientation (Cell orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getPher (int type) {
+        return pher[type];
+    }
+
+    public void setHomePher (int pher, int type) {
+        this.pher[type] = pher;
+    }
+
+    public boolean isNest () {
+        return isNest;
+    }
+
+    public void setNest (boolean isNest) {
+        this.isNest = isNest;
+    }
+
+    public boolean isFood () {
+        return isFood;
+    }
+
+    public void setFood (boolean isFood) {
+        this.isFood = isFood;
+    }
 
     public int getCurrState () {
         return currState;
--- CellSociety/src/ForagingAntsRules.java
+++ CellSociety/src/ForagingAntsRules.java
@@ -1,3 +1,5 @@
+import java.awt.Point;
+
 /**
  * This class, which extends SimulationRules, controls the state updates of each cell in the
  * simulation when a SegregationRules configuration file has been loaded in the program. It is
@@ -12,8 +14,10 @@
 
 public class ForagingAntsRules extends SimulationRules {
     private static final int paramNeeded = 0;
-    public static final int DEAD = 1;
-    public static final int ALIVE = 2;
+    public static final int NO_FOOD_ANT = 1;
+    public static final int FOOD_ANT = 2;
+    private int homePherInd = 0;
+    private int foodPherInd = 1;
 
     public ForagingAntsRules () {
         super();
@@ -25,9 +29,45 @@ public class ForagingAntsRules extends SimulationRules {
      */
     @Override
     protected int findNextState (Cell curr, Cell[] neighbors, Cell[][] grid) {
-     return 0;
+        if (checkState(curr, EMPTY)) {
+            return EMPTY;
+        }
+        else if (checkState(curr, NO_FOOD_ANT)) {
+            findFood(curr,neighbors,grid);
+        }
+        else{
+            returnToNest(curr,neighbors,grid);
+        }
+        return EMPTY;
     }
 
+    private void findFood(Cell curr, Cell[] neighbors, Cell[][] grid){
+        
+    }
+    
+    private void returnToNest(Cell curr, Cell[] neighbors, Cell[][] grid){
+        if(curr.isFood()){
+            curr.setOrientation(findNextOrientation(curr,neighbors,homePherInd));
+        }
+        //TODO finish implementation
+        
+    }
+    
+    private Cell findNextOrientation(Cell curr, Cell[] neighbors, int pherType){
+        int maxPher = 0;
+        int maxCellIndex = 0;
+        for(int i=0;i<neighbors.length;i++){
+            if(neighbors[i].getPher(pherType)>maxPher){
+                maxPher=neighbors[i].getPher(pherType);
+                maxCellIndex = i;
+            }
+        }
+        if(maxPher == 0){
+            return null;
+        }
+        return neighbors[maxCellIndex];
+    }
+    
     @Override
     protected void setSimulationParameters (String[] simParams) {
         // No Parameters to Set for this Simulation
