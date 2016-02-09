cac3780849bf212ca6f91179204d08cab2de56bd
HUIJIA YU <hy91@duke.edu>
2016-02-08 23:12:07 -0500

    fix hex and tri displays

--- CellSociety/src/TriDisplay.java
+++ CellSociety/src/TriDisplay.java
@@ -10,26 +10,26 @@ public class TriDisplay extends Display{
     @Override
     public void initDisplay () {
         // TODO Auto-generated method stub
-    	double CELL_WIDTH = DISPLAY_WIDTH / Math.ceil(getRows()/2.); 
-        double CELL_HEIGHT = DISPLAY_HEIGHT / getColumns();
+    	double CELL_WIDTH = DISPLAY_WIDTH /Math.ceil(getColumns()/2.) ; 
+        double CELL_HEIGHT = DISPLAY_HEIGHT / getRows();
         initColors();
         for (int i = 0; i < getRows(); i++) {
             for (int j = 0; j < getColumns(); j++) {
                 getDisplay()[i][j] = new Polygon();
                 getDisplay()[i][j].setStroke(Color.BLACK);
-                double k = (i)/2.;
+                double k = (j)/2.;
                 //System.out.println(k);
                 if((i+j)%2==0){
                 	getDisplay()[i][j].getPoints().addAll(new Double[]{
-                		(CELL_WIDTH * k + X_OFFSET), (CELL_HEIGHT * j + Y_OFFSET),
-                		(CELL_WIDTH * (k+.5) + X_OFFSET), (CELL_HEIGHT * (j+1) + Y_OFFSET),
-                	    (CELL_WIDTH * (k+1) + X_OFFSET), (CELL_HEIGHT * j + Y_OFFSET) });
+                		(CELL_WIDTH * k + X_OFFSET), (CELL_HEIGHT * i + Y_OFFSET),
+                		(CELL_WIDTH * (k+.5) + X_OFFSET), (CELL_HEIGHT * (i+1) + Y_OFFSET),
+                	    (CELL_WIDTH * (k+1) + X_OFFSET), (CELL_HEIGHT * i + Y_OFFSET) });
                 }
                 else{
                 	getDisplay()[i][j].getPoints().addAll(new Double[]{
-	                		(CELL_WIDTH * (k) + X_OFFSET), (CELL_HEIGHT * (j+1) + Y_OFFSET),
-	                		(CELL_WIDTH * (k+.5) + X_OFFSET), (CELL_HEIGHT * j + Y_OFFSET),
-	                	    (CELL_WIDTH * (k+1) + X_OFFSET), (CELL_HEIGHT * (j+1) + Y_OFFSET) });
+	                		(CELL_WIDTH * (k) + X_OFFSET), (CELL_HEIGHT * (i+1) + Y_OFFSET),
+	                		(CELL_WIDTH * (k+.5) + X_OFFSET), (CELL_HEIGHT * i + Y_OFFSET),
+	                	    (CELL_WIDTH * (k+1) + X_OFFSET), (CELL_HEIGHT * (i+1) + Y_OFFSET) });
                 }
             }
         }
