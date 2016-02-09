1b96ca64039d9337447a67db884b99507e67cce8
Jin An <sa190@duke.edu>
2016-02-08 20:36:08 -0500

    XML Shape Edits

--- CellSociety/src/HexDisplay.java
+++ CellSociety/src/HexDisplay.java
@@ -5,31 +5,29 @@ public class HexDisplay extends Display{
 
     public HexDisplay (int rows, int columns, int states) {
         super(rows, columns, states);
+        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void initDisplay () {
-    	double CELL_WIDTH = DISPLAY_WIDTH / getRows()*4/3;
-		double CELL_HEIGHT = DISPLAY_HEIGHT / (getColumns()+.5);
+        // TODO Auto-generated method stub
+    	int CELL_WIDTH = DISPLAY_WIDTH / getRows();
+		int CELL_HEIGHT = DISPLAY_HEIGHT / getColumns();
 		initColors();
 		for (int i = 0; i < getRows(); i++) {
 			for (int j = 0; j < getColumns(); j++) {
 				getDisplay()[i][j] = new Polygon();
 				getDisplay()[i][j].setStroke(Color.BLACK);
-				double k = i*.75;
-				double l = j+.5*(i%2);
-				
+				double k = i+.5*j%2;
 				getDisplay()[i][j].getPoints().addAll(new Double[]{
-						(CELL_WIDTH * (k) + X_OFFSET), (CELL_HEIGHT * (l+.5) + Y_OFFSET),
-						(CELL_WIDTH * (k+.25) + X_OFFSET), (CELL_HEIGHT * (l) + Y_OFFSET),
-						(CELL_WIDTH * (k+.75) + X_OFFSET), (CELL_HEIGHT * (l) + Y_OFFSET), 
-						(CELL_WIDTH * (k+1) + X_OFFSET), (CELL_HEIGHT * (l+.5) + Y_OFFSET),
-						(CELL_WIDTH * (k+.75) + X_OFFSET), (CELL_HEIGHT * (l+1) + Y_OFFSET),
-						(CELL_WIDTH * (k+.25) + X_OFFSET), (CELL_HEIGHT * (l+1) + Y_OFFSET)
-						
-						  });
+						(CELL_WIDTH * i + X_OFFSET), (CELL_HEIGHT * (j+.25) + Y_OFFSET),
+						(CELL_WIDTH * (i+.5) + X_OFFSET), (CELL_HEIGHT * (j) + Y_OFFSET),
+						(CELL_WIDTH * (i+1) + X_OFFSET), (CELL_HEIGHT * (j+.25) + Y_OFFSET), 
+						(CELL_WIDTH * i + X_OFFSET), (CELL_HEIGHT * (j+.75) + Y_OFFSET),
+						(CELL_WIDTH * (i+.5) + X_OFFSET), (CELL_HEIGHT * (j+1) + Y_OFFSET),
+						(CELL_WIDTH * (i+1) + X_OFFSET), (CELL_HEIGHT * (j+.75) + Y_OFFSET)  });
 
-			}
+            }
 		}
     }
 
