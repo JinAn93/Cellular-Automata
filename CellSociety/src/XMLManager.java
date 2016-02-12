f1b3cfd73daeac19fd6903955b68e171ddd55c80
Jin An <sa190@duke.edu>
2016-02-12 01:06:11 -0500

    file saved path fixed

--- CellSociety/src/XMLManager.java
+++ CellSociety/src/XMLManager.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -16,8 +17,10 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+
 /**
- * XMLReader serves to read XML files and return a concatenated string of relevant information extracted 
+ * XMLReader serves to read XML files and return a concatenated string of relevant information
+ * extracted
  * from the XML file. Its readXMLFile method interacts with UserInterface class
  * 
  * @author Joseph Lilien
@@ -28,7 +31,10 @@ import org.w3c.dom.NodeList;
 public class XMLManager {
     private String RESOURCE_PACKAGE_XML = "Resources/XMLTags";
     public static final int NO_ERROR = 0;
-    public static final List<String> errorTypes = Arrays.asList("No_Error","No_Simulation_Type", "Parameter_Error", "Invalid_Cell_State", "Grid_Init_Error");
+    public static final List<String> errorTypes = Arrays.asList("No_Error", "No_Simulation_Type",
+                                                                "Parameter_Error",
+                                                                "Invalid_Cell_State",
+                                                                "Grid_Init_Error");
     public static final int NO_SIMULATION_TYPE = 1;
     public static final int PARAMETER_ERROR = 2;
     public static final int INVALID_CELL_STATE = 3;
@@ -37,8 +43,10 @@ public class XMLManager {
 
     /**
      * Called from UserInterface class. This method gets the loaded file and returns a concatenated
-     * String of information separated by ','. It frequently calls getElements method which is implemented
-     * within the class. 
+     * String of information separated by ','. It frequently calls getElements method which is
+     * implemented
+     * within the class.
+     * 
      * @param file
      * @return
      */
@@ -76,72 +84,88 @@ public class XMLManager {
         }
         return strsimInfo;
     }
-    
-    public void writeXMLFile(String name, String title, String author, int shape, int edge, int numStates, String setting, int row, int column, String initial){
-        try{
+
+    public void writeXMLFile (String name,
+                              String title,
+                              String author,
+                              int shape,
+                              int edge,
+                              int numStates,
+                              String setting,
+                              int row,
+                              int column,
+                              String initial) {
+        try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-            
+
             Document doc = docBuilder.newDocument();
-            Element rootElement = createElements(doc,"SIMULATION");
+            Element rootElement = createElements(doc, "SIMULATION");
             doc.appendChild(rootElement);
-            
-            rootElement.appendChild(addElements(doc,"NAME",name));
-            rootElement.appendChild(addElements(doc,"TITLE",title));
-            rootElement.appendChild(addElements(doc,"AUTHOR",author));
-            rootElement.appendChild(addElements(doc,"SHAPE",Integer.toString(shape)));
-            rootElement.appendChild(addElements(doc,"EDGE",Integer.toString(edge)));
-            rootElement.appendChild(addElements(doc,"NUMSTATES",Integer.toString(numStates)));
-            rootElement.appendChild(addElements(doc,"SETTING",setting));
-            rootElement.appendChild(addElements(doc,"DIMENSION",Integer.toString(row) + "x" + Integer.toString(column)));
-            rootElement.appendChild(addElements(doc,"INITIAL",initial));
-            
-            
+
+            rootElement.appendChild(addElements(doc, "NAME", name));
+            rootElement.appendChild(addElements(doc, "TITLE", title));
+            rootElement.appendChild(addElements(doc, "AUTHOR", author));
+            rootElement.appendChild(addElements(doc, "SHAPE", Integer.toString(shape)));
+            rootElement.appendChild(addElements(doc, "EDGE", Integer.toString(edge)));
+            rootElement.appendChild(addElements(doc, "NUMSTATES", Integer.toString(numStates)));
+            rootElement.appendChild(addElements(doc, "SETTING", setting));
+            rootElement.appendChild(addElements(doc, "DIMENSION", Integer.toString(row) + "x" +
+                                                                  Integer.toString(column)));
+            rootElement.appendChild(addElements(doc, "INITIAL", initial));
+
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("C:\\Users\\owner\\filename.xml"));
-            
+            StreamResult result = new StreamResult(new File("savedFile.xml"));
+
             transformer.transform(source, result);
             System.out.println("File saved!");
         }
-        catch (ParserConfigurationException pce){
+        catch (ParserConfigurationException pce) {
             pce.printStackTrace();
         }
-        catch (TransformerException tfe){
+        catch (TransformerException tfe) {
             tfe.printStackTrace();
         }
     }
-    
-    private Element addElements(Document doc, String s, String content){
-        Element element = createElements(doc,s);
+
+    private Element addElements (Document doc, String s, String content) {
+        Element element = createElements(doc, s);
         element.appendChild(doc.createTextNode(content));
         return element;
     }
-    public String checkError(int row, int column, String whichSim, int[] gridInit, String[] parameters){
-        if(!Time.simulations.contains(whichSim)){
+
+    public String checkError (int row,
+                              int column,
+                              String whichSim,
+                              int[] gridInit,
+                              String[] parameters) {
+        if (!Time.simulations.contains(whichSim)) {
             return errorTypes.get(NO_SIMULATION_TYPE);
         }
-        if(gridInit.length != row * column){
+        if (gridInit.length != row * column) {
             return errorTypes.get(GRID_INIT_ERROR);
         }
-        for(int i=0; i<gridInit.length; i++){
-            if(gridInit[i] < 0 || gridInit[i] > 3){
+        for (int i = 0; i < gridInit.length; i++) {
+            if (gridInit[i] < 0 || gridInit[i] > 3) {
                 return errorTypes.get(INVALID_CELL_STATE);
             }
         }
-        
+
         int simIndex = Time.simulations.lastIndexOf(whichSim);
-        if(CellManager.myPossibleRules[simIndex].isInvalid(parameters)){
+        if (CellManager.myPossibleRules[simIndex].isInvalid(parameters)) {
             return errorTypes.get(PARAMETER_ERROR);
         }
         return errorTypes.get(NO_ERROR);
     }
 
     /**
-     * getElements serves to avoid repeated codes that get Elements in XML by tag Name. By using resources folder,
+     * getElements serves to avoid repeated codes that get Elements in XML by tag Name. By using
+     * resources folder,
      * mistakes from string input can be avoided.
+     * 
      * @param eElement
      * @param s
      * @return
@@ -149,9 +173,9 @@ public class XMLManager {
     private String getElements (Element eElement, String s) {
         return (eElement.getElementsByTagName(myResources.getString(s)).item(0).getTextContent());
     }
-    
-    private Element createElements (Document doc, String s){
+
+    private Element createElements (Document doc, String s) {
         return (doc.createElement(myResources.getString(s)));
-        
+
     }
 }
