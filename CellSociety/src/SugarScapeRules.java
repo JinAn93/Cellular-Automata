5d25639ce930ef48106f2425771bccbf3bc79682
Jin An <sa190@duke.edu>
2016-02-14 16:37:08 -0500

    SugarScaper updates

--- CellSociety/src/SugarScapeRules.java
+++ CellSociety/src/SugarScapeRules.java
@@ -1,6 +1,6 @@
 /**
  * This class, which extends SimulationRules, controls the state updates of each cell in the
- * simulation when a SegregationRules configuration file has been loaded in the program. It is
+ * simulation when a patch configuration file has been loaded in the program. It is
  * dependent upon the general methods defined in SimulationRules and contains methods that run the
  * Cell updates according to the specific rules defined by the SugarScape simulation.
  * 
@@ -11,12 +11,13 @@
  */
 
 public class SugarScapeRules extends SimulationRules {
-    private static final int paramNeeded = 3;
-    private static final int AGENT = 1;
-    private int amountSugar = 0;
-    private int maxSugarCap = 1;
-    private int currSugar = 2;
-    private double initSugar, sugarMetabol, vision;
+    private static final int paramNeeded = 5;
+    private static final int AGENT_STATE = 1;
+    private int amountSugarIndex = 0;
+    private int maxSugarCapIndex = 1;
+    private int sugarGrowBackCountIndex = 2;
+    private int agentSugarIndex = 3;
+    private int initSugar, sugarMetabol, vision, sugarGrowBackInterval, sugarGrowBackRate;
 
     public SugarScapeRules () {
         super();
@@ -29,24 +30,31 @@ public class SugarScapeRules extends SimulationRules {
     @Override
     protected int findNextState (Cell curr, Cell[] neighbors, Cell[][] grid, int shape) {
         handleUserInput(curr);
-        if (checkState(curr, EMPTY)) {
-            return EMPTY;
-        }
-        else if (checkState(curr, AGENT)) {
+        if (checkState(curr, AGENT_STATE)) {
             return handleAgent(curr, neighbors, grid);
+        }
+        else {
+            if (isSugarMax(curr)) {
+                // return the max sugar
+            }
+            else {
+                // check the sugarGrowBack interval, if it's time, increase the sugar and return new
+                // sugar or not return its sugar amount. If new sugar amount is greater than max,
+                // set it to max
+            }
         }
         return EMPTY;
     }
 
     @Override
     protected void handleUserInput (Cell curr) {
-        //work on this
+        // This method allows users to allocate the agents
     }
 
     private int handleAgent (Cell curr, Cell[] neighbors, Cell[][] grid) {
         if (isSugarAround(curr, neighbors)) {
-            findMaxSugar(curr, neighbors);
-            moveToSugar(curr, neighbors);
+            int maxSugarIndex = findMaxSugar(curr, neighbors);
+            moveToSugar(curr, neighbors, maxSugarIndex);
         }
         else {
             return moveRandom(neighbors);
@@ -55,12 +63,28 @@ public class SugarScapeRules extends SimulationRules {
         return 0;
     }
 
-    private void findMaxSugar (Cell curr, Cell[] neighbors) {
-        
+    /**
+     * findMaxSugar iterates through neighbors to find out the max sugar location. If there are more
+     * than one, use the smallest index using the characteristic of how neighbor is set up. (The
+     * greater the index, the farther it is from the cell)
+     * 
+     * @param curr
+     * @param neighbors
+     */
+    private int findMaxSugar (Cell curr, Cell[] neighbors) {
+        return 0;
     }
 
-    private void moveToSugar (Cell curr, Cell[] neighbors) {
-
+    /**
+     * Set the neighbors[loc]'s nextstate to agent and reduce its sugar amount by 1
+     * 
+     * @param curr
+     * @param neighbors
+     * @param loc
+     */
+    private void moveToSugar (Cell curr, Cell[] neighbors, int loc) {
+        // change the nextstate of neighbors[loc] to agent and pass the agentSugar parameter to new
+        // location
     }
 
     private int moveRandom (Cell[] neighbors) {
@@ -71,32 +95,68 @@ public class SugarScapeRules extends SimulationRules {
         return randomLoc;
     }
 
-    private boolean isValidMove (int loc, Cell[] neighbors) {
-        return !checkState(neighbors[loc], BLOCKED) && !checkState(neighbors[loc], AGENT);
+    private boolean isSugarMax (Cell curr) {
+        return (curr.getCellParamList().get(amountSugarIndex) != curr.getCellParamList()
+                .get(maxSugarCapIndex));
     }
 
-    private boolean isSugarAround (Cell curr, Cell[] neighbors) {
+    private boolean isValidMove (int loc, Cell[] neighbors) {
+        return !checkState(neighbors[loc], BLOCKED) && !checkState(neighbors[loc], AGENT_STATE);
+    }
 
+    /**
+     * returns boolean to check whether there is sugar within the cell's neighbors.
+     * 
+     * @param curr
+     * @param neighbors
+     * @return
+     */
+    private boolean isSugarAround (Cell curr, Cell[] neighbors) {
+        for (int i = 0; i < neighbors.length; i++) {
+            if (neighbors[i].getCellParamList().get(amountSugarIndex) > 0) {
+                return true;
+            }
+        }
         return false;
     }
 
     private void sugarMeta (Cell curr) {
-        curr.getCellParamList().set(currSugar,
-                                    curr.getCellParamList().get(currSugar) - sugarMetabol);
+        // if its agentSugar is greater than sugarMeta, return the remaining
+        // if it's less than sugarMeta, return empty because it dies
+    }
+
+    /**
+     * This method overrides existing setNeighbors method in super class to apply vision to increase
+     * the capacity of neighbor. We have to consider whether the CellGrid will have OutofBounds
+     * Exception if vision is greater than 1. The order would be: {North+1, West+1, South+1, East+1,
+     * North+2...}
+     * 
+     * @param cellGrid
+     * @param i
+     * @param j
+     * @param shape
+     * @return
+     */
+    @Override
+    public Cell[] setNeighbors (Cell[][] cellGrid, int i, int j, int shape) {
+        return new Cell[] { cellGrid[i][j] };
     }
 
     @Override
     protected void setSimulationParameters (String[] simParams) {
-        initSugar = Double.parseDouble(simParams[0]);
-        sugarMetabol = Double.parseDouble(simParams[1]);
-        vision = Double.parseDouble(simParams[2]);
+        initSugar = Integer.parseInt(simParams[0]);
+        sugarMetabol = Integer.parseInt(simParams[1]);
+        vision = Integer.parseInt(simParams[2]);
+        sugarGrowBackInterval = Integer.parseInt(simParams[3]);
+        sugarGrowBackRate = Integer.parseInt(simParams[4]);
     }
 
     @Override
     protected void initializeCellParams (Cell curr) {
-        curr.getCellParamList().add((double) 0); // amount sugar
-        curr.getCellParamList().add((double) 0); // max sugar cap
-        curr.getCellParamList().add((double) 0); // agent sugar
+        curr.getCellParamList().add((double) 0); // amount sugar for patch
+        curr.getCellParamList().add((double) 0); // max sugar cap for patch
+        curr.getCellParamList().add((double) 0); // sugarGrowBackInterval
+        curr.getCellParamList().add((double) 0); // agent sugar (if there exists agent in the cell)
     }
 
     @Override
